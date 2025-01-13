328e564d1af72b8c67dcda8fb040a6459247a216
qwer0114 <rnalgh0114@gmail.com>
2025-01-14 00:14:37 +0900

    📦 chore: client 절대 경로 처리

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src') },
+      { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+      { find: '@components', replacement: path.resolve(__dirname, './src/components') },
+      { find: '@constants', replacement: path.resolve(__dirname, './src/constants') },
+      { find: '@hooks', replacement: path.resolve(__dirname, './src/hooks') },
+      { find: '@page', replacement: path.resolve(__dirname, './src/page') },
+      { find: '@stores', replacement: path.resolve(__dirname, './src/stores') },
+      { find: '@types', replacement: path.resolve(__dirname, './src/types') },
+      { find: '@utils', replacement: path.resolve(__dirname, './src/utils') },
+    ],
+  },
+});
