a4b65ddc1f3c37019290adce4d3428c595b296a5
qwer0114 <rnalgh0114@gmail.com>
2025-01-13 19:22:12 +0900

    📦 chore: story book content 경로 추가

--- apps/storybook/tailwind.config.ts
+++ apps/storybook/tailwind.config.ts
@@ -1,8 +1,8 @@
-import sharedConfig from "@repo/tailwind-config";
-import type { Config } from "tailwindcss";
+import sharedConfig from '@repo/tailwind-config';
+import type { Config } from 'tailwindcss';
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/**/*.tsx"],
+const config: Pick<Config, 'content' | 'presets'> = {
+  content: ['./src/**/*.tsx', '../../apps/**/*.tsx'],
   presets: [sharedConfig],
 };
 
