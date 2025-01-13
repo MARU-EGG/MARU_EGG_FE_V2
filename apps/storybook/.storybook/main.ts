c2bac9196d2cfa4ffe2d001f29242a667352c140
qwer0114 <rnalgh0114@gmail.com>
2025-01-14 00:17:09 +0900

    📦 chore: storybook 절대경로 처리

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,25 +1,40 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
+import { dirname, join, resolve } from 'path';
+import { mergeConfig } from 'vite';
 
-import { join, dirname } from "path";
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-postcss"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-postcss'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: [
+          { find: '@', replacement: resolve(__dirname, '../../client/src') },
+          { find: '@assets', replacement: resolve(__dirname, '../../client/src/assets') },
+          { find: '@components', replacement: resolve(__dirname, '../../client/src/components') },
+          { find: '@constants', replacement: resolve(__dirname, '../../client/src/constants') },
+          { find: '@hooks', replacement: resolve(__dirname, '../../client/src/hooks') },
+          { find: '@page', replacement: resolve(__dirname, '../../client/src/page') },
+          { find: '@stores', replacement: resolve(__dirname, '../../client/src/stores') },
+          { find: '@types', replacement: resolve(__dirname, '../../client/src/types') },
+          { find: '@utils', replacement: resolve(__dirname, '../../client/src/utils') },
+        ],
+      },
+    });
+  },
 };
+
 export default config;
