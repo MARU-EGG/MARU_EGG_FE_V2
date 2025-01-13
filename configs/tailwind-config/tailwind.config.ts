61280feda1e9a473be6af5426e4fc1eac35be8b7
qwer0114 <rnalgh0114@gmail.com>
2025-01-13 19:19:46 +0900

    💄 style: 디자인 시스템 정의

--- configs/tailwind-config/tailwind.config.ts
+++ configs/tailwind-config/tailwind.config.ts
@@ -1,10 +1,27 @@
 import type { Config } from "tailwindcss";
 
-// We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#002968",
+          bg: "rgba(0, 41, 104, 0.06)",
+        },
+        category_border: "#EBEBEB",
+      },
+      fontSize: {
+        body1: ["12px", { lineHeight: "24px", fontWeight: "400" }],
+        body2: ["12px", { lineHeight: "24px", fontWeight: "500" }],
+        body3: ["16px", { fontWeight: "500" }],
+        title: ["16px", { fontWeight: "700" }],
+      },
+      fontFamily: {
+        sans: ["Pretendard", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
+
 export default config;
