a104a37a8cfa4f3538ddfd4eddefb108dbb30a5a
qwer0114 <rnalgh0114@gmail.com>
2025-01-14 20:51:11 +0900

    💄 style: 배경 색 추가

--- apps/client/src/components/selector/selector.tsx
+++ apps/client/src/components/selector/selector.tsx
@@ -3,7 +3,9 @@ import SelectorHeader from '@/components/selector/select-header';
 import SelectOption from '@/components/selector/select-option';
 
 function Selector({ children }: { children: React.ReactNode }) {
-  return <div className="border-category_border w-56 divide-y overflow-hidden rounded-lg border">{children}</div>;
+  return (
+    <div className="border-category_border w-56 divide-y overflow-hidden rounded-lg border bg-white">{children}</div>
+  );
 }
 
 Selector.Header = SelectorHeader;
