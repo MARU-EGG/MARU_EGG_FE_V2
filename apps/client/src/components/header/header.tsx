6fd7ea4bb29787fcc121b0446b540c0a28fdff77
qwer0114 <rnalgh0114@gmail.com>
2025-01-14 23:09:52 +0900

    💄 style: header 컴포넌트 높이 조절

--- apps/client/src/components/header/header.tsx
+++ apps/client/src/components/header/header.tsx
@@ -3,7 +3,7 @@ import IconButton from '@/components/icon-button/icon-button';
 
 function Header() {
   return (
-    <header className="sticky top-0 z-10 h-14 w-full bg-white">
+    <header className="sticky top-0 z-10 h-16 w-full bg-white">
       <div className="flex h-full items-center justify-center px-4">
         <h1 className="text-primary text-title flex-1 text-center">명지대 입학처 챗봇</h1>
         <IconButton aria-label="새로고침">
