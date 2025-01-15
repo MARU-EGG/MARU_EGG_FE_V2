0ab1119dc1f92c87b4129a137c31f9f5afd3e87e
qwer0114 <rnalgh0114@gmail.com>
2025-01-15 15:34:51 +0900

    💄 style: tailwind 사용자 지정 애니메이션 추가

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,9 +1,53 @@
 import sharedConfig from '@repo/tailwind-config';
 import type { Config } from 'tailwindcss';
 
-const config: Pick<Config, 'content' | 'presets'> = {
+const config: Pick<Config, 'content' | 'presets' | 'theme'> = {
   content: ['./src/**/*.tsx'],
   presets: [sharedConfig],
+  theme: {
+    extend: {
+      keyframes: {
+        fadein: {
+          '0%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
+        fadeout: {
+          '0%': {
+            opacity: '1',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+        slideUp: {
+          '0%': {
+            transform: 'translateY(100%)',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+          },
+        },
+        slideDown: {
+          '0%': {
+            transform: 'translateY(0)',
+          },
+          '100%': {
+            transform: 'translateY(100%)',
+          },
+        },
+      },
+      animation: {
+        fadein: 'fadein 0.25s',
+        fadeout: 'fadeout 0.25s',
+        slideUp: 'slideUp 0.25s ease-out',
+        slideDown: 'slideDown 0.25s ease-out',
+      },
+    },
+  },
 };
 
 export default config;
