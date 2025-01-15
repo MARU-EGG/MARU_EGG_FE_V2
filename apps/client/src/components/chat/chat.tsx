524496586e3991bff08b6abcaebe492185e0d1d8
qwer0114 <rnalgh0114@gmail.com>
2025-01-15 12:24:43 +0900

    ♻️ refactor: chat 컴포넌트 system, user 분리

--- apps/client/src/components/chat/chat.tsx
+++ apps/client/src/components/chat/chat.tsx
@@ -5,27 +5,32 @@ interface ChatProps {
   children: React.ReactNode;
 }
 
-function Chat({ role, children }: ChatProps) {
-  const styles = {
-    system: {
-      container: 'flex gap-2 relative',
-      message: 'bg-white text-black',
-    },
-    user: {
-      container: 'flex gap-2 justify-end',
-      message: 'bg-primary text-white',
-    },
-  };
-
+function AdminChat({ children }: { children: React.ReactNode }) {
   return (
-    <div className={`${styles[role].container}`}>
-      {role === 'system' ? <MaruIcon className="absolute -left-9 top-6" /> : null}
-      <div className="flex flex-col gap-2">
-        {role === 'system' ? <div className="px-3 text-xs text-gray-500">마루에그</div> : null}
-        <div className={`max-w-72 rounded-lg px-3 py-2 text-sm ${styles[role].message}`}>{children}</div>
+    <div className="relative flex">
+      <MaruIcon className="absolute -left-9 top-6" />
+      <div className="flex flex-col gap-1">
+        <div className="px-3 text-xs text-gray-500">마루에그</div>
+        <span className="max-w-72 whitespace-pre-wrap break-words rounded-lg bg-white px-3 py-2 text-sm">
+          {children}
+        </span>
       </div>
     </div>
   );
+}
+
+function UserChat({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex justify-end gap-2">
+      <span className="bg-primary whitespace-pre-wrap break-words rounded-lg px-3 py-2 text-sm text-white">
+        {children}
+      </span>
+    </div>
+  );
+}
+
+function Chat({ role, children }: ChatProps) {
+  return role === 'system' ? <AdminChat>{children}</AdminChat> : <UserChat>{children}</UserChat>;
 }
 
 export default Chat;
