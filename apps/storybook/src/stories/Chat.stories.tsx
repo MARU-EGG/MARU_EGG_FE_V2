2f0638e543586126f3eea9f1ee71a47f2346aa0e
qwer0114 <rnalgh0114@gmail.com>
2025-01-15 12:25:20 +0900

    💄 style: 스토리북 테스트 메시지 변경

--- apps/storybook/src/stories/Chat.stories.tsx
+++ apps/storybook/src/stories/Chat.stories.tsx
@@ -6,7 +6,9 @@ const meta = {
   component: Chat,
   parameters: {
     layout: 'centered',
+    backgrounds: { default: 'light' },
   },
+
   tags: ['autodocs'],
   argTypes: {},
 } satisfies Meta<typeof Chat>;
@@ -17,7 +19,8 @@ type Story = StoryObj<typeof Chat>;
 export const SystemMessage: Story = {
   args: {
     role: 'system',
-    children: '안녕하세요! 무엇을 도와드릴까요?',
+    children:
+      '안녕하세요,\n명지대학교 입시 정보를 똑똑하게\n찾아주는 마루에그입니다!\n\n명지대학교 입시에 대해 궁금하신가요?\n아래 전형 중 하나를 선택해주세요!',
   },
 };
 
